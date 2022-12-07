f4037dca6fc181c5ea4adb48207f6ee6c8cc2591
KPInc425 <KPInc425@gmail.com>
2022-12-07 15:34:43 -0800

    Implement exercise 3.22 solution: Lint config and cleanup

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 const phoneNumberEntry = require('./models/phoneNumberEntry.js');
 
 const requestLogger = (request, response, next) => {
-    console.log(request);
-    console.log('Method: ', request.method);
-    console.log('Path: ', request.path);
-    console.log('Body: ', request.body);
-    console.log('---');
-    next();
+  console.log(request);
+  console.log('Method: ', request.method);
+  console.log('Path: ', request.path);
+  console.log('Body: ', request.body);
+  console.log('---');
+  next();
 }
 
 app.use(express.json());
@@ -21,36 +21,38 @@ app.use(express.static('build'));
 
 
 app.get('/api/persons', (request, response) => {
-    phoneNumberEntry.find({}).then((person) => {
-      response.json(person);
-    })
+  phoneNumberEntry.find({}).then((person) => {
+    response.json(person);
+  })
 })
 
 app.get('/info', async (request, response) => {
-    // console.log(phoneNumberEntry);
+  // console.log(phoneNumberEntry);
 
-    const count = await phoneNumberEntry.countDocuments({}).then((request, response) => {
-      console.log(request);
+  // eslint-disable-next-line no-unused-vars
+  const count = await phoneNumberEntry.countDocuments({}).then((request, response) => {
+    console.log(request);
 
-      return request;
-    })
+    return request;
+  })
 
-    response.send(`<p>Phonebook has info for ${ count } people</p>
+  response.send(`<p>Phonebook has info for ${ count } people</p>
                    <p> ${ new Date()} </p>`);
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    phoneNumberEntry.findById(request.params.id).then((person) => {
-        response.json(person);
-    })
+  phoneNumberEntry.findById(request.params.id).then((person) => {
+    response.json(person);
+  })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    phoneNumberEntry.findByIdAndRemove(request.params.id)
-      .then((result) => {
-        response.status(204).end()
-      })
-      .catch((error) => next(error));
+  phoneNumberEntry.findByIdAndRemove(request.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error));
 })
 
 const generateID = () => {
@@ -85,17 +87,17 @@ app.post('/api/persons', (request, response, next) => {
   console.log(newPerson);
 
   phoneNumberEntry
-    .exists({ name: newPerson.name})
+    .exists({ name: newPerson.name })
     .then(bool => {
       if (bool) {
-          response.status(403).send({ error: `A person named ${newPerson.name} is already in the PhoneBook`})
+        response.status(403).send({ error: `A person named ${newPerson.name} is already in the PhoneBook` })
       } else {
-          newPerson.save()
+        newPerson.save()
           .then((savedEntry) => {
             response.json(savedEntry);
           })
           .catch((error) => next(error))
-        }
+      }
     })
 
 })
@@ -104,31 +106,31 @@ app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body;
 
   phoneNumberEntry.findByIdAndUpdate(
-    request.params.id, 
-    { name, number }, 
+    request.params.id,
+    { name, number },
     { new: true, runValidators: true, context: 'query' })
     .then(updatedNumber => {
       response.json(updatedNumber);
     })
     .catch((error) => next(error));
-}) 
+})
 
 const unknownEndPoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' });
+  response.status(404).send({ error: 'unknown endpoint' });
 }
 
 app.use(unknownEndPoint);
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
+  console.error(error.message);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === "ValidationError") {
-        return response.status(400).json({ error: error.message });
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
 
-    next(error);
+  next(error);
 }
 
 app.use(errorHandler);
@@ -136,5 +138,5 @@ app.use(errorHandler);
 const PORT = 3005;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 })
--- models/phoneNumberEntry.js
+++ models/phoneNumberEntry.js
@@ -1,42 +1,45 @@
 const mongoose = require('mongoose');
 
+
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI;
 
 console.log('connecting to', url);
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connnecting to MongoDB: ', error.message);
-    })
+  // eslint-disable-next-line no-unused-vars
+  .then(result => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connnecting to MongoDB: ', error.message);
+  })
 
 const phoneEntrySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: (v) => {
-                return /^\d{2,3}-\d+/.test(v); 
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        }, 
-        required: true,
-    }
+    required: true,
+  }
 })
 
 phoneEntrySchema.set('toJSON', {
-    transform: ( document, returnedObject ) => {
-        returnedObject.id = returnedObject._id;
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+  transform: ( document, returnedObject ) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
 })
 
 module.exports = mongoose.model('Person', phoneEntrySchema);
